b497984cd21cd92d539355b6513521207a26bcf4
Hoàng Phú Nam <hoangphu.nam0604@gmail.com>
2024-08-01 09:41:30 +0700

    update

--- auto-cack/event.js
+++ auto-cack/event.js
@@ -1,4 +1,57 @@
 
-  const username = document.querySelector(".hello-user-left .f-myriadpro_b_cond").innerHTML;
-
-  console.log(username.textContent());
+  if($(".hello-user-left .f-myriadpro_b_cond").length > 0){
+    username = $(".hello-user-left .f-myriadpro_b_cond").text();
+    chrome.storage.local.set({username});
+    doiMayChu();
+  }
+  
+  function doiMayChu(){
+   $.ajax({
+      type: "post",
+      url: "https://cuuam.gosu.vn/home/su-kien/sinh-nhat-11-tuoi.doi-may-chu.htm",
+      dataType: "json",
+      data: {
+          server: "S1"
+      },
+      success: function (result) {
+        alertCustomer(result.message);
+        nhanQua3k();
+      }
+    }); 
+  }
+    
+function nhanQua3k(){
+  $.post('https://cuuam.gosu.vn/home/su-kien/sinh-nhat-11-tuoi.xu-ly-loi-chuc.htm', {type: 'loi-chuc-3k'}, function (res) {
+    alertCustomer(res.message);
+    nhanQua5k();
+}, 'json');
+}
+function nhanQua5k(){
+  $.post('https://cuuam.gosu.vn/home/su-kien/sinh-nhat-11-tuoi.xu-ly-loi-chuc.htm', {type: 'loi-chuc-5k'}, function (res) {
+    alertCustomer(res.message);
+    nhanQua9k();
+  }, 'json');
+}
+function nhanQua9k(){
+  $.post('https://cuuam.gosu.vn/home/su-kien/sinh-nhat-11-tuoi.xu-ly-loi-chuc.htm', {type: 'loi-chuc-9k'}, function (res) {
+    alertCustomer(res.message);    
+    nhanQua11k();
+  }, 'json');
+}
+function nhanQua9k(){
+  $.post('https://cuuam.gosu.vn/home/su-kien/sinh-nhat-11-tuoi.xu-ly-loi-chuc.htm', {type: 'loi-chuc-11k'}, function (res) {
+    alertCustomer(res.message);    
+    setTimeout(function(){
+      doiTaiKhoan();
+    },3000);
+  }, 'json');
+}
+function doiTaiKhoan(){
+  document.location = $(".islogin-gameleft a").attr('href');
+}
+function alertCustomer(text) {
+  if (text != '') {
+      $("#popup-alert .detail-alert .text-alert").html(text);
+      $('#popup-alert').fadeIn();
+  }
+}
--- auto-cack/login.js
+++ auto-cack/login.js
@@ -1,1 +1,17 @@
-alert('Login' )
+chrome.storage.local.get(['username'], async function (result) {
+  let username = result.username
+  let clone_account = username.includes('eventcack')
+  if (clone_account) {
+    let lastid = parseInt(username.replace(/^\D+/g, ''));
+    const account_number = String(lastid).padStart(3, '0')
+    const account = `eventcack${account_number}`;
+    const password = `asd123456`;
+    console.log(account);
+    document.getElementsByName(`UserName`)[1].value = account;
+    document.getElementsByName(`Password`)[1].value = password;
+    document.getElementsByTagName('form')[1].submit();
+    setTimeout(function(){
+      document.getElementsByTagName('form')[1].submit();
+    },10000)
+  }
+}); 
